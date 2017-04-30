99c63b4876e688d20f75e2926af9948b69effb51
lionel1704 <lionel1704@gmail.com>
2017-04-30 13:26:04 +0530

    added login module

--- message/urls.py
+++ message/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     url(r'^(?P<num>[0-9]+)/$', views.detail, name='detail'),
     url(r'^(?P<num>[0-9]+)/submit/$', views.submit, name='submit'),
     url(r'^dearsenior/$', views.dear, name='dear'),
-    url(r'^dearsenior/(?P<num>[0-9]+)/$', views.viewmsg, name='viewmsg'),
+    url(r'^dearsenior/reg/$', views.reg, name='reg'),
+    url(r'^dearsenior/login/$', views.login, name='login'),
+    url(r'^dearsenior/reg/show$', views.regviewmsg, name='regviewmsg'),
+    url(r'^dearsenior/login/show/$', views.loginviewmsg, name='loginviewmsg'),
 ]
--- message/views.py
+++ message/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth import authenticate
 
 from .models import List, Bubye
+from django.contrib.auth.models import User
 
 
 def index(request):
@@ -22,11 +24,36 @@ def submit(request, num):
     return render(request, 'message/thankyou.html')
 
 def dear(request):
-    final_list = List.objects.all()
-    context = {'final_list': final_list}
-    return render(request, 'message/dearsenior.html', context)
+    return render(request, 'message/dearsenior.html')
 
-def viewmsg(request, num):
-    messages = Bubye.objects.filter(alumini=num)
-    nam = List.objects.get(id=num)
+def reg(request):
+    items = List.objects.all()
+    context = {"items": items}
+    return render(request, 'message/signup.html', context)
+
+def login(request):
+    items = List.objects.all()
+    context = {"items": items}
+    return render(request, 'message/login.html', context)
+
+def regviewmsg(request):
+    usrid = request.POST['usr']
+    pwd = request.POST['pwd']
+    nam = List.objects.get(id=usrid)
+    messages = Bubye.objects.filter(alumini=usrid)
+    obj = User.objects.create_user(first_name=nam.name, username=usrid, password=pwd)
+    obj.save()
     return render(request, 'message/viewmsg.html', {'messages': messages, 'nam': nam})
+
+def loginviewmsg(request):
+    usrid = request.POST['usr']
+    pwd = request.POST['pwd']
+    user = authenticate(username = usrid, password=pwd)
+    nam = List.objects.get(id=usrid)
+    messages = Bubye.objects.filter(alumini=usrid)
+    if user is not None:
+        return render(request, 'message/viewmsg.html', {'messages': messages, 'nam': nam})
+    else:
+        items = List.objects.all()
+        context = {"items": items}
+        return render(request, 'message/login1.html', context)
